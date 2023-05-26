a74ed6a58efd98065a99293629b943a9007fcdf7
erikqb3 <erikqb3@gmail.com>
2023-05-25 18:25:46 -0600

    trying to fix stuff

--- junk-red/src/index.js
+++ junk-red/src/index.js
@@ -5,8 +5,48 @@ import './scss/landing.css';
 import {LogoNav, Nav}from './js/nav_comp';
 import {Landing} from './js/landing_pg';
 import reportWebVitals from './reportWebVitals';
+import { helperFunctions } from './js/helperFunctions';
 
+import { useRef } from "react";
 import {videoInRibbon} from './assets/imgs/img_landing';
+
+
+function Ribbon (){
+  const pageList = ["characterDesign","video","about"];
+  const currentPage = helperFunctions.getCurrentPage(pageList);
+  
+  const videoRef = useRef();
+  const setPlayBack = ()=> {
+    videoRef.current.playbackRate = 0.5;
+  }
+
+  let ribbon_tag;
+  
+  
+  if (currentPage == "index"){
+    ribbon_tag = 
+    <div id="ribbon">
+      <video 
+        autoPlay={true} 
+        loop={true} 
+        muted={true}
+        ref={videoRef}
+        onCanPlay={()=> setPlayBack()}>
+        <source src={videoInRibbon} ></source>
+      </video>
+    </div>
+  }
+  else {
+    ribbon_tag=
+      <div id="ribbon"></div>
+  }
+
+  
+  return (
+    ribbon_tag
+  )
+}
+
 
 
 const main = 
@@ -14,15 +54,7 @@ const main =
     <LogoNav></LogoNav>
     <Landing></Landing>
     <Nav></Nav>
-    <div id="ribbon">
-      {/* <video 
-        autoplay="true" 
-        loop="true" 
-        muted="true"
-        playbackRate="0.5">
-        <source src={videoInRibbon} ></source>
-      </video> */}
-    </div>
+    <Ribbon></Ribbon>
   </main>
 
 
